b3f7bb04f7be80276b00a7d49eb6202922c55d48
Rasaboun <rayane.amie@gmail.com>
2023-04-20 03:29:57 +0200

    Change Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,31 +21,31 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-gradient-to-bl from-slate-900 via-slate-800 to-slate-900 min-h-screen">
         <nav className="fixed inset-x-0 bottom-0 text-black flex flex-row justify-center lg:justify-between items-center mx-5 mb-5 ">
-          <b className="hidden lg:block text-2xl font-bold bg-gradient-to-tl from-gray-400 via-zinc-500 to-neutral-700 text-transparent bg-clip-text">
+          <b className="hidden lg:block text-2xl font-bold bg-gradient-to-tl from-gray-600 via-slate-500 to-cyan-400 text-transparent bg-clip-text">
             Rasaboun
           </b>
           <div className="font-semibold text-slate-800 text-lg flex flex-row items-center rounded-3xl border-gray-300 border-2 p-2 space-x-2 lg:space-x-8 shadow-2xl shadow-gray-950/60 backdrop-blur-md bg-white/80">
             <Link
               href="/"
-              className="hover:border-gray-500 border-2 border-transparent rounded-2xl p-2 text-transparent bg-clip-text bg-gradient-to-l from-gray-500 via-zinc-500 to-neutral-700"
+              className="hover:border-gray-500 border-2 border-transparent rounded-2xl p-2 text-transparent bg-clip-text bg-gradient-to-l from-gray-500 via-zinc-600 to-slate-800"
             >
               Home
             </Link>
             <Link
               href="/Blog"
-              className="hover:border-gray-500 border-2 border-transparent rounded-2xl p-2 text-transparent bg-clip-text bg-gradient-to-bl from-gray-500 via-zinc-500 to-neutral-700"
+              className="hover:border-gray-500 border-2 border-transparent rounded-2xl p-2 text-transparent bg-clip-text bg-gradient-to-bl from-gray-500 via-zinc-600 to-slate-800"
             >
               Blog
             </Link>
             <Link
               href="/Portfolio"
-              className="hover:border-gray-500 border-2 border-transparent rounded-2xl p-2 text-transparent bg-clip-text bg-gradient-to-br from-gray-500 via-zinc-500 to-neutral-700"
+              className="hover:border-gray-500 border-2 border-transparent rounded-2xl p-2 text-transparent bg-clip-text bg-gradient-to-br from-gray-500 via-zinc-600 to-slate-800"
             >
               Portfolio
             </Link>
             <a
               href="https://github.com/Rasaboun"
-              className="hover:border-gray-500 border-2 border-transparent rounded-2xl p-2 text-transparent bg-clip-text bg-gradient-to-r from-gray-500 via-zinc-500 to-neutral-700"
+              className="hover:border-gray-500 border-2 border-transparent rounded-2xl p-2 text-transparent bg-clip-text bg-gradient-to-r from-gray-500 via-zinc-600 to-slate-800"
             >
               Github
             </a>
