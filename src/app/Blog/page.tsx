a8e8c5c9ecbd6b5de540a5918fae1c9ed315d8ec
Rasaboun <rayane.amie@gmail.com>
2023-05-09 00:12:33 +0200

    fix blog img error 2

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -1,18 +1,43 @@
-import Logo from "./react-icon.svg"
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return <main className="flex flex-col space-y-8 justify-center items-center h-screen mx-8">
-
-    <Link href="/Blog/SSR">
-    <Image src={Logo} alt="react logo" className="rounded-lg w-64 shadow-lg shadow-blue-500 p-4"/>
-    </Link>
-    <h1 className="text-white">The Benefits of Server-Side Rendering (SSR) for Web Development</h1>
-
-    <Link href="/Blog/SSR">
-    <Image src={Logo} alt="react logo" className="rounded-lg w-64 shadow-lg shadow-white p-4"/>
-    </Link>
-    <h1 className="text-white">Les avantages du rendu côté serveur (SSR) pour le développement web</h1>
-  </main>;
+  return (
+    <main className="flex flex-col space-y-8 justify-center items-center h-screen mx-8">
+      <Link href="/Blog/SSR">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="-11.5 -10.23174 23 20.46348"
+          className="rounded-lg w-64 shadow-lg shadow-blue-500 p-4"
+        >
+          <title>React Logo</title>
+          <circle cx="0" cy="0" r="2.05" fill="#61dafb" />
+          <g stroke="#61dafb" strokeWidth="0.9" fill="none">
+            <ellipse rx="11" ry="4.2" />
+            <ellipse rx="11" ry="4.2" transform="rotate(60)" />
+            <ellipse rx="11" ry="4.2" transform="rotate(120)" />
+          </g>
+        </svg>
+      </Link>
+      <h1 className="text-white text-lg">
+        The Benefits of Server-Side Rendering (SSR) for Web Development
+      </h1>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="-11.5 -10.23174 23 20.46348"
+        className="rounded-lg w-64 shadow-lg shadow-white p-4"
+      >
+        <title>React Logo</title>
+        <circle cx="0" cy="0" r="2.05" fill="#61dafb" />
+        <g stroke="#61dafb" strokeWidth="0.9" fill="none">
+          <ellipse rx="11" ry="4.2" />
+          <ellipse rx="11" ry="4.2" transform="rotate(60)" />
+          <ellipse rx="11" ry="4.2" transform="rotate(120)" />
+        </g>
+      </svg>
+      <Link href="/Blog/SSR"></Link>
+      <h1 className="text-white text-lg">
+        Les avantages du rendu côté serveur (SSR) pour le développement web
+      </h1>
+    </main>
+  );
 }
