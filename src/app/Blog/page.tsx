cb95a725359023395367e4963e422b4ad3ad1fc7
Rasaboun <rayane.amie@gmail.com>
2023-05-09 00:02:15 +0200

    Add SSR Blog Post + FR

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -1,8 +1,18 @@
+import Logo from "./react-icon.svg"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main className="flex justify-center items-center h-screen">
+  return <main className="flex flex-col space-y-8 justify-center items-center h-screen mx-8">
 
-    <span className="text-3xl text-slate-400 font-semibold">Coming soon ...</span>
+    <Link href="/Blog/SSR">
+    <Image src={Logo} alt="react logo" className="rounded-lg w-64 shadow-lg shadow-blue-500 p-4"/>
+    </Link>
+    <h1 className="text-white">The Benefits of Server-Side Rendering (SSR) for Web Development</h1>
 
+    <Link href="/Blog/SSR">
+    <Image src={Logo} alt="react logo" className="rounded-lg w-64 shadow-lg shadow-white p-4"/>
+    </Link>
+    <h1 className="text-white">Les avantages du rendu côté serveur (SSR) pour le développement web</h1>
   </main>;
 }
