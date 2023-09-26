11f3c7b35a6c4728083727a0f4ff48441b11d3af
Rasaboun <rayane.amie@gmail.com>
2023-09-26 19:12:28 +0200

    fix end

--- rasaboun/tailwind.config.cjs
+++ rasaboun/tailwind.config.cjs
@@ -8,34 +8,19 @@ module.exports = {
     "./src/**/*.{ts,tsx,astro}",
   ],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
+
     extend: {
       keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
+
         openning: {
           from: { opacity: 0 },
           to: { opacity: 1 },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
+
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
         "openning": "openning 0.7s ease-in-out"
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
 };
