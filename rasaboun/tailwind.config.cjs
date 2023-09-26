b4ce01992f560610380f7310326486d032dcf01b
Rasaboun <rayane.amie@gmail.com>
2023-09-26 18:07:09 +0200

    first block

---
+++ rasaboun/tailwind.config.cjs
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx,astro}",
+    "./components/**/*.{ts,tsx,astro}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx,astro}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        openning: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "openning": "openning 0.7s ease-in-out"
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
