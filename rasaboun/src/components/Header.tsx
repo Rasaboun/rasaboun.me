51ca0cc46d828fc5dae79cc332a888dddafb3123
Rasaboun <rayane.amie@gmail.com>
2023-10-03 16:28:10 +0200

    style: change header mobile

--- rasaboun/src/components/Header.tsx
+++ rasaboun/src/components/Header.tsx
@@ -1,14 +1,19 @@
+import img from "./images/github-mark.png"
 export default function Header() {
-  let now = new Date().toUTCString().slice(0,16)
+  let now = new Date().toUTCString().slice(0, 16);
   return (
-    <div className="flex flex-row justify-between mx-16 mt-6 items-end border-b-[1px] pb-2 border-slate-300    ">
-      <div className="flex flex-col space-y-2">
+    <div className="flex flex-row justify-between mx-4 md:mx-16 mt-6 items-end border-b-[1px] pb-2 border-slate-300    ">
+      <div className="flex flex-col w-fit space-y-2">
         <p className="text-slate-500 uppercase text-sm">{now}</p>
-        <p className="text-slate-900 font-semibold text-4xl">Welcome to my Portfolio</p>
+        <p className="text-slate-900 font-semibold text-3xl md:text-4xl">
+          Welcome to my Portfolio
+        </p>
       </div>
-      <div>
-        <p className="p-1 px-3 bg-slate-200 rounded-full">R</p>
-      </div>
+  
+        <a href="https://github.com/Rasaboun">
+        <img src={img.src} alt="github logo" className="w-8 h-8 hover:scale-105 transition-transform duration-150 ease-in-out"/>
+        </a>
+    
     </div>
   );
 }
