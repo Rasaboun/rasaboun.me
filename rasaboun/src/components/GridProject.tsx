d691bef1eed649b53798252ab3df5bd1c647ef67
Rasaboun <rayane.amie@gmail.com>
2023-10-17 12:00:25 +0200

    add: cal.com

--- rasaboun/src/components/GridProject.tsx
+++ rasaboun/src/components/GridProject.tsx
@@ -1,6 +1,6 @@
 export default function GridProject(props: any) {
   return (
-    <div className="max-w-7xl grid grid-cols-1 md:grid-cols-2 gap-14 mx-4 md:mx-16 grid-rows-6 mt-8">
+    <div className="max-w-7xl grid grid-cols-1 md:grid-cols-2  gap-14 mx-4 md:mx-16 grid-rows-6 mt-10">
       <a
         href="/me"
         className="h-[280px] bg-indigo-600 col-span-1 w-full row-span-6	items-center	justify-center	  relative hover:scale-105 active:scale-100 transition-transform	duration-300	ease-in-out	shadow-2xl rounded-xl"
